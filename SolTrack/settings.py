5a25b34a113a001c9b21c9fe1027ac60e28cf255
AldrinMedina <medina.aldrin02@gmail.com>
2025-10-06 08:14:15 +0800

    m

--- SolTrack/settings.py
+++ SolTrack/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
+
 
 load_dotenv()  # Only needed if you are using a local .env file
 
@@ -89,18 +91,28 @@ WSGI_APPLICATION = 'SolTrack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),  # must be your Supabase host, e.g., db.<hash>.supabase.co
-        'PORT': os.getenv('DB_PORT', '5432'),
-        "OPTIONS": {"sslmode": "require"},  # important for Supabase
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),  # must be your Supabase host, e.g., db.<hash>.supabase.co
+#         'PORT': os.getenv('DB_PORT', '5432'),
+#         "OPTIONS": {"sslmode": "require"},  # important for Supabase
+#     }
+# }
 
+
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+        options="-c client_encoding=UTF8"
+    )
+}
 
 
 # Password validation
