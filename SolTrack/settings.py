5c7d311a1f2cab936413d76446ec8a4dfb48d820
AldrinMedina <medina.aldrin02@gmail.com>
2025-11-29 04:40:13 +0800

    asd

--- SolTrack/settings.py
+++ SolTrack/settings.py
@@ -59,13 +59,15 @@ INSTALLED_APPS = [
     'adminpanel'
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('MAILER_EMAIL')
 EMAIL_HOST_PASSWORD = os.getenv('MAILER_PASSWORD')
-DEFAULT_FROM_EMAIL = 'SolTrack <noreply@localhost>'
+DEFAULT_FROM_EMAIL = 'SolTrack <noreply@gmail.com>'
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
 
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
