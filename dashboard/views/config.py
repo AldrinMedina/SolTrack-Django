df60b2af47f94f7200eaf4e52a087396920888a4
AldrinMedina <medina.aldrin02@gmail.com>
2025-12-31 20:05:51 +0800

    Update config.py

--- dashboard/views/config.py
+++ dashboard/views/config.py
@@ -4,19 +4,31 @@ import os
 from web3 import Web3
 
 # ----------------------------------------------------------------------
-# CONFIG
+# RPC / NETWORK
 # ----------------------------------------------------------------------
-CHAIN_RPC =  os.getenv("GANACHE_URL")
-DEPLOYER_PRIVATE_KEY =  os.getenv("DEPLOYER_PRIVATE_KEY")
-DEPLOYER_ADDRESS = os.getenv("DEPLOYER_ADDRESS")
-SOLC_VERSION = "0.8.19"
-DEFAULT_GAS_LIMIT = int(os.getenv("DEFAULT_GAS_LIMIT", 3000000))
-#CHAIN_ID = int(os.getenv("CHAIN_ID"))
+rpc_url = os.getenv("TESTNET_RPC_URL", "http://127.0.0.1:7545")
+
+if not rpc_url:
+    raise RuntimeError("TESTNET_RPC_URL is not set")
+
+# Detect chain
 CHAIN_ID = 11155111 if "sepolia" in rpc_url.lower() else 1337
+
+# ----------------------------------------------------------------------
+# DEPLOYER
+# ----------------------------------------------------------------------
+DEPLOYER_PRIVATE_KEY = os.getenv("DEPLOYER_PRIVATE_KEY")
+DEPLOYER_ADDRESS = os.getenv("DEPLOYER_ADDRESS")
+
+# ----------------------------------------------------------------------
+# GAS / FEES
+# ----------------------------------------------------------------------
+SOLC_VERSION = "0.8.19"
+DEFAULT_GAS_LIMIT = int(os.getenv("DEFAULT_GAS_LIMIT", 3_000_000))
 MAX_FEE_GWEI = int(os.getenv("MAX_FEE_GWEI", 6))
 PRIORITY_FEE_GWEI = int(os.getenv("PRIORITY_FEE_GWEI", 1))
-GAS_PRICE_GWEI = 5
-rpc_url = os.getenv("TESTNET_RPC_URL", "http://127.0.0.1:7545")
+GAS_PRICE_GWEI = int(os.getenv("GAS_PRICE_GWEI", 5))
+
 # ----------------------------------------------------------------------
 # WEB3 INSTANCE
 # ----------------------------------------------------------------------
